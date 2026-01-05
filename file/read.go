c56eaca0536e1f93fc353a200b0e35330976e964
lhdht <15229684090@163.com>
2026-01-05 11:50:22 +0800

    fix: 修改注释

--- file/read.go
+++ file/read.go
@@ -6,6 +6,9 @@ import (
 )
 
 // ReadRemoteFile 读取远程文件内容
+// url: 远程文件的 URL 地址
+// 返回: 文件内容的字节切片。如果下载失败或读取失败，返回 error
+// 注意：此函数会将整个文件加载到内存，不适合读取大文件
 func ReadRemoteFile(url string) ([]byte, error) {
 	res, err := http.Get(url)
 	if err != nil {
