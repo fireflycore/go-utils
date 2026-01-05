56204b6739cdff6c9fe172a54303d1902b3ca324
lhdht <15229684090@163.com>
2026-01-05 10:39:26 +0800

    feature: crypto

---
+++ crypto/md5.go
@@ -0,0 +1,26 @@
+package crypto
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+)
+
+type MD5 struct{}
+
+func NewMD5() *MD5 {
+	return &MD5{}
+}
+
+func (ist *MD5) Encrypt(input, key string) string {
+	hash := md5.New()
+	if key != "" {
+		_, _ = io.WriteString(hash, key)
+	}
+	_, _ = io.WriteString(hash, input)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func (ist *MD5) Compare(input, salt, hash string) bool {
+	return ist.Encrypt(input, salt) == hash
+}
